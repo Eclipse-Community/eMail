f68f4cf1ef0aec6eba31fb804f3779019eb8952f
K4sum1 <k4sum1@eclipse.cx>
2024-06-22 00:04:28 -0700

    Basic Navigator branding

---
+++ navigator/branding/official/configure.sh
@@ -0,0 +1,23 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+MOZ_APP_NAME="Lun3r Navigator"
+
+MOZ_APP_BASENAME="Lun3r Navigator"
+
+MOZ_APP_DISPLAYNAME="Lun3r Navigator"
+
+MOZ_APP_VENDOR="Eclipse Community"
+
+MOZ_APP_FULLNAME="Lun3r Navigator"
+
+MOZ_APP_PROFILE="Eclipse Community/Lun3r Navigator"
+
+MOZ_APP_REMOTINGNAME=com.eclipse.lun3r.navigator
+
+MOZ_DISTRIBUTION_ID=com.eclipse.lun3r.navigator
+
+MOZ_MACBUNDLE_NAME="Lun3r Navigator.app"
+
+MOZ_SOURCE_REPO=https://github.com/Eclipse-Community/eMail
