26cda67f39ba1144792e31c1b20058e64ae886b4
K4sum1 <k4sum1@eclipse.cx>
2024-07-14 06:35:29 -0700

    other

--- navigator/branding/official/configure.sh
+++ navigator/branding/official/configure.sh
@@ -2,13 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_NAME="Lun3r_Navigator"
+MOZ_APP_NAME="lun3r-navigator"
 
 MOZ_APP_BASENAME="Lun3r Navigator"
 
 MOZ_APP_DISPLAYNAME="Lun3r Navigator"
 
-MOZ_APP_UA_NAME=Lun3r_Navigator
+MOZ_APP_UA_NAME=Lun3r-Navigator
 
 MOZ_APP_VENDOR="Eclipse Community"
 
